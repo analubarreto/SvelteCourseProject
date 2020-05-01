93c396fd821479f3b3e505e36cc5e50799d2d0ec
analubarreto <analuibm@gmail.com>
2020-04-30 23:54:42 -0300

    added favorite badge

---
+++ src/UI/Badge.svelte
@@ -0,0 +1,17 @@
+<style>
+  span {
+    display: inline-block;
+    margin: 0 .25rem;
+    border-radius: 3px;
+    border: 1px solid #cf0056;
+    background: #cf0056;
+    color: white;
+    padding: 0 .5rem;
+    font-family: 'Lato', sans-sarif;
+    font-size: .8rem;
+  }
+</style>
+
+<span>
+  <slot />
+</span>
